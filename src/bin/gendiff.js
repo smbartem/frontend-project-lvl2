85d0d4e261eafcbb66a65295bbfe249d37b30c74
smbartem <artembass88@yandex.ru>
2020-09-11 15:56:59 +0300

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node --experimental-json-modules
 
 import program from 'commander';
-import pack from '../package.json';
+import pack from '../../package.json';
+import genDiff from '../index.js';
 
 program
   .description(pack.description)
   .helpOption('-h, --help', 'read more information')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((file1, file2) => {
+    const difference = genDiff(file1, file2);
+    console.log(difference);
+  });
 
 program.version(pack.version, '-v, --version');
 
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+const genDiff = (file1, file2) => {
+  const beforeFile = fs.readFileSync(path.resolve(process.cwd(), file1), 'utf8');
+  const afterFile = fs.readFileSync(path.resolve(process.cwd(), file2), 'utf8');
+  const parseBeforeFile = JSON.parse(beforeFile);
+  const parseAfterFile = JSON.parse(afterFile);
+  const keys1 = _.keys(parseBeforeFile);
+  const keys2 = _.keys(parseAfterFile);
+  const keys = _.union(keys1, keys2).sort();
+  let resultOfGenDiff = '';
+  keys.forEach((key) => {
+    if (!_.has(parseAfterFile, key)) {
+      resultOfGenDiff += `  - ${key}: ${parseBeforeFile[key]}\n`;
+    } else if (!_.has(parseBeforeFile, key)) {
+      resultOfGenDiff += `  + ${key}: ${parseAfterFile[key]}\n`;
+    } else if (parseBeforeFile[key] !== parseAfterFile[key]) {
+      resultOfGenDiff += `  - ${key}: ${parseBeforeFile[key]}\n`;
+      resultOfGenDiff += `  + ${key}: ${parseAfterFile[key]}\n`;
+    } else {
+      resultOfGenDiff += `    ${key}: ${parseBeforeFile[key]}\n`;
+    }
+  });
+  return `{ \n${resultOfGenDiff}}`;
+};
+
+export default genDiff;
