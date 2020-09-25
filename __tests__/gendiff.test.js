aa2534e1d5819f8db7d79e7bcb0dd5b8353b2ac6
smbartem <artembass88@yandex.ru>
2020-09-25 17:20:17 +0300

    test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,24 @@
 import fs from 'fs';
 import path from 'path';
-import { expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const readFile = (file) => fs.readFileSync(path.resolve(process.cwd(), file), 'utf8');
-const fileExtensionsList = ['json', 'yaml', 'ini'];
-const resultFormatStyleList = ['stylish', 'plain', 'json'];
 
-resultFormatStyleList.forEach((format) => {
-  const ResultFile = readFile(`__tests__/__fixtures__/${format}FormateResult`);
-  fileExtensionsList.forEach((extension) => {
-    const pathToFile1 = `__tests__/__fixtures__/treeFile1.${extension}`;
-    const pathToFile2 = `__tests__/__fixtures__/treeFile2.${extension}`;
-    expect(genDiff(pathToFile1, pathToFile2, format)).toBe(ResultFile);
-  });
+test.each([
+  ['json', 'stylish'],
+  ['yml', 'stylish'],
+  ['ini', 'stylish'],
+  ['json', 'plain'],
+  ['yml', 'plain'],
+  ['ini', 'plain'],
+  ['json', 'json'],
+  ['yml', 'json'],
+  ['ini', 'json'],
+])('Testing gendiff', (formatOfFile, formatOfResult) => {
+  const pathToFile1 = `__tests__/__fixtures__/treeFile1.${formatOfFile}`;
+  const pathToFile2 = `__tests__/__fixtures__/treeFile2.${formatOfFile}`;
+  const results = ['__tests__/__fixtures__/jsonFormateResult.json', '__tests__/__fixtures__/plainFormateResult.txt', '__tests__/__fixtures__/stylishFormateResult.txt'];
+  const nameOfResultFile = results.filter((result) => result.includes(`${formatOfResult}`)).join();
+  const expected = readFile(nameOfResultFile);
+  expect(genDiff(pathToFile1, pathToFile2, formatOfResult)).toEqual(expected);
 });
