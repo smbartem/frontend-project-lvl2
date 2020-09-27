c92d58b83a851e88aa71172d20dd700f7c415829
smbartem <artembass88@yandex.ru>
2020-09-27 17:30:43 +0300

    mentor's comments

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,50 +1,52 @@
-const gap = 2;
-const getSpace = (qty) => {
-  const space = ' ';
-  return space.repeat(qty);
+const getSpace = (depth) => {
+  const space = '    ';
+  return space.repeat(depth);
 };
 
-const formatValue = (value, space) => {
-  const objectToString = (object, space2) => {
-    const iter = (node, space3) => {
-      const entries = Object.entries(node);
-      const result = entries.flatMap((child) => {
-        const [key, val] = child;
-        if (typeof val !== 'object') {
-          return `${getSpace(space3 + gap)}${key}: ${val}`;
-        }
-        return `${getSpace(space3 + gap)}${key}: ${iter(val, space3 + gap + gap)}`;
-      });
-      return `{\n${result.join('\n')}\n${getSpace(space3 - gap)}}`;
-    };
-    return iter(object, space2 + gap);
+const nestedValueToString = (object, depth1) => {
+  const iter = (node, depth2) => {
+    const innerValue = Object.entries(node).flatMap(([key, val]) => {
+      if (typeof val !== 'object') {
+        return `${getSpace(depth2 + 1)}${key}: ${val}`;
+      }
+      return `${getSpace(depth2 + 1)}${key}: ${iter(val, depth2 + 1)}`;
+    });
+    return `{\n${innerValue.join('\n')}\n${getSpace(depth2)}}`;
   };
-  const result = (typeof value !== 'object') ? value : objectToString(value, space + gap);
-  return result;
+  return iter(object, depth1);
+};
+
+const formatValue = (value, depth) => {
+  if (typeof value === 'object') {
+    return nestedValueToString(value, depth + 1);
+  }
+  return value;
 };
 
 const stylish = (diffTree) => {
   const iter = (node, depth) => {
-    const space = depth * gap;
-    const treeOfString = node.flatMap((child) => {
+    const stylishValue = node.flatMap((child) => {
       switch (child.status) {
         case 'deleted':
-          return `${getSpace(space)}- ${child.key}: ${formatValue(child.value, space)}`;
+          return `${getSpace(depth)}  - ${child.key}: ${formatValue(child.value, depth)}`;
         case 'added':
-          return `${getSpace(space)}+ ${child.key}: ${formatValue(child.value, space)}`;
+          return `${getSpace(depth)}  + ${child.key}: ${formatValue(child.value, depth)}`;
         case 'unmodified':
-          return `${getSpace(space)}  ${child.key}: ${formatValue(child.value, space)}`;
+          return `${getSpace(depth)}    ${child.key}: ${formatValue(child.value, depth)}`;
         case 'modified':
-          return [`${getSpace(space)}- ${child.key}: ${formatValue(child.previousValue, space)}`, `${getSpace(space)}+ ${child.key}: ${formatValue(child.presentValue, space)}`];
+          return [
+            `${getSpace(depth)}  - ${child.key}: ${formatValue(child.previousValue, depth)}`,
+            `${getSpace(depth)}  + ${child.key}: ${formatValue(child.presentValue, depth)}`,
+          ];
         case 'nested':
-          return `${getSpace(space)}  ${child.key}: ${iter(child.treeChild, depth + gap)}`;
+          return `${getSpace(depth)}    ${child.key}: ${iter(child.treeChild, depth + 1)}`;
         default:
           throw new Error('Error! Type is invalid');
       }
     });
-    return `{\n${treeOfString.join('\n')}\n${getSpace(space - gap)}}`;
+    return `{\n${stylishValue.join('\n')}\n${getSpace(depth)}}`;
   };
-  return iter(diffTree, 1);
+  return iter(diffTree, 0);
 };
 
 export default stylish;
